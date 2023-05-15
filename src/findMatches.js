773f47a2b90e2323aaa1b6bd0dee4a19d794723a
schevcha <schevcha.work@gmail.com>
2023-05-15 14:54:36 +0300

    modified findMatches

--- src/findMatches.js
+++ src/findMatches.js
@@ -1,13 +1,13 @@
 // Создать функцию, которая принимает на вход два массива чисел и возвращает массив из элементов, которые есть в обоих массивах.
 
 const findMatches = (arr1, arr2) => {
-    return arr1.reduce((acc, current) => {
+    const result = arr1.reduce((acc, current) => {
         if (arr2.includes(current)) {
             acc.push(current)
         }
-        // FIXME: Это должно быть после выполнения всех итераций reduce
-        return Array.from(new Set(acc))
+        return acc
     }, [])
+    return Array.from(new Set(result))
 }
 
 module.exports = findMatches;
